7b1125c3c84a6d23dce3347613aebab5cc9c075e
Ingo von Brunn <repo@public-files.de>
2025-05-27 16:58:14 +0200

    added associations and validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,9 @@
 class Category < ApplicationRecord
+  # associations
+  has_many :books
+
+  # validations
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,9 @@
 class Request < ApplicationRecord
+  # associations
   belongs_to :book
   belongs_to :user
+
+  # validations
+  validates :status, numericality: { only_integer: true }
+
 end
