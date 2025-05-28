3d5849c73eb94a6753096fe02961a414c9ef15a9
Ingo von Brunn <repo@public-files.de>
2025-05-28 14:29:07 +0200

    added stimulus controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,12 @@
 class BooksController < ApplicationController
   def index
+
+    # The `geocoded` scope filters only books with coordinates
+  @markers = @books.geocoded.map do |book|
+    {
+      lat: flat.latitude,
+      lng: flat.longitude
+    }
+    end
   end
 end
