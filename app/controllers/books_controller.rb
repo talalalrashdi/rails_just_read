3dd7c44dbc9141dcd890db39c9b2e6b469a20ab9
ChiaJung Chung <heavenly0430@gmail.com>
2025-05-28 14:16:22 +0200

    controller-index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,30 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Bookk.new
+    @review = Review.new(list: @list)
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(list_params)
+    if @book.save
+      redirect_to list_path(@book)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:book).permit(:title, :description, :author, :category, :user)
   end
 end
